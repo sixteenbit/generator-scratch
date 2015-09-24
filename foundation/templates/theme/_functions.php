a06d55de74aba0173cc82676b2511ef496ddc5f8
Justin Peacock <justin@sixteenbit.com>
2015-09-24 14:46:22 -0400

    updated function vars

--- foundation/templates/theme/_functions.php
+++ foundation/templates/theme/_functions.php
@@ -1,23 +1,23 @@
 <?php
 
 // Useful global constants
-define( 'FARO_VERSION', '0.1.0' );
-define( 'FARO_URL', get_stylesheet_directory_uri() );
-define( 'FARO_TEMPLATE_URL', get_template_directory_uri() );
-define( 'FARO_PATH', get_template_directory() . '/' );
-define( 'FARO_INC', FARO_PATH . 'inc/' );
+define( '<%= opts.funcPrefix.toUpperCase() %>_VERSION', '0.1.0' );
+define( '<%= opts.funcPrefix.toUpperCase() %>_URL', get_stylesheet_directory_uri() );
+define( '<%= opts.funcPrefix.toUpperCase() %>_TEMPLATE_URL', get_template_directory_uri() );
+define( '<%= opts.funcPrefix.toUpperCase() %>_PATH', get_template_directory() . '/' );
+define( '<%= opts.funcPrefix.toUpperCase() %>_INC', <%= opts.funcPrefix.toUpperCase() %>_PATH . 'inc/' );
 
 /**
  * <%= opts.projectTitle %> only works in WordPress 4.2 or later.
  */
 if ( version_compare( $GLOBALS['wp_version'], '4.2', '<' ) ) {
-	require FARO_TEMPLATE_URL . '/inc/back-compat.php';
+	require <%= opts.funcPrefix.toUpperCase() %>_TEMPLATE_URL . '/inc/back-compat.php';
 }
 
 // Includes
-require_once FARO_INC . 'core.php';
-require_once FARO_INC . 'custom-header.php';
-require_once FARO_INC . 'template-tags.php';
-require_once FARO_INC . 'extras.php';
-require_once FARO_INC . 'jetpack.php';
-require_once FARO_INC . 'walker.php';
+require_once <%= opts.funcPrefix.toUpperCase() %>_INC . 'core.php';
+require_once <%= opts.funcPrefix.toUpperCase() %>_INC . 'custom-header.php';
+require_once <%= opts.funcPrefix.toUpperCase() %>_INC . 'template-tags.php';
+require_once <%= opts.funcPrefix.toUpperCase() %>_INC . 'extras.php';
+require_once <%= opts.funcPrefix.toUpperCase() %>_INC . 'jetpack.php';
+require_once <%= opts.funcPrefix.toUpperCase() %>_INC . 'walker.php';
