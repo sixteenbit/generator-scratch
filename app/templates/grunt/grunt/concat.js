8175924ea09e24b783c2b37505ff144f1560a055
Justin Peacock <justin@sixteenbit.com>
2016-12-20 09:27:31 -0500

    initial update

--- app/templates/grunt/grunt/concat.js
+++ app/templates/grunt/grunt/concat.js
@@ -18,14 +18,40 @@ module.exports = {
 	},
 	main: {
 		src: [
-			'bower_components/foundation-sites/dist/foundation.js'
+			// Libraries required by Foundation
+			'bower_components/motion-ui/dist/motion-ui.js',
+			'bower_components/what-input/what-input.js',
+
+			// Core Foundation files
+			'bower_components/foundation-sites/js/foundation.core.js',
+			'bower_components/foundation-sites/js/foundation.util.*.js',
+
+			// Individual Foundation components
+			// If you aren't using a component, just remove it from the list
+			'bower_components/foundation-sites/js/foundation.abide.js',
+			'bower_components/foundation-sites/js/foundation.accordion.js',
+			'bower_components/foundation-sites/js/foundation.accordionMenu.js',
+			'bower_components/foundation-sites/js/foundation.drilldown.js',
+			'bower_components/foundation-sites/js/foundation.dropdown.js',
+			'bower_components/foundation-sites/js/foundation.dropdownMenu.js',
+			'bower_components/foundation-sites/js/foundation.equalizer.js',
+			'bower_components/foundation-sites/js/foundation.interchange.js',
+			'bower_components/foundation-sites/js/foundation.magellan.js',
+			'bower_components/foundation-sites/js/foundation.offcanvas.js',
+			'bower_components/foundation-sites/js/foundation.orbit.js',
+			'bower_components/foundation-sites/js/foundation.responsiveMenu.js',
+			'bower_components/foundation-sites/js/foundation.responsiveToggle.js',
+			'bower_components/foundation-sites/js/foundation.reveal.js',
+			'bower_components/foundation-sites/js/foundation.slider.js',
+			'bower_components/foundation-sites/js/foundation.sticky.js',
+			'bower_components/foundation-sites/js/foundation.tabs.js',
+			'bower_components/foundation-sites/js/foundation.toggler.js',
+			'bower_components/foundation-sites/js/foundation.tooltip.js'
 		],
 		dest: 'assets/js/foundation.js'
 	},
 	scripts: {
 		src: [
-			'bower_components/motion-ui/dist/motion-ui.js',
-			'bower_components/what-input/what-input.js',
 			'assets/js/src/_*.js'
 		],
 		dest: 'assets/js/scripts.js'
